96b7d149efff9f36065c3a3079461eaf8c5fe99a
OleksandrLukomskyi <LUKOMSKYI.OLEKSANDR@GMAIL.COM>
2024-06-08 07:50:23 +0200

    add store

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,17 @@ import "modern-normalize";
 import './index.css'
 import { ThemeProvider } from './providers/ThemeProvider.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    
+    <Provider store={store}> 
       <ThemeProvider>
-
         <App />
-
       </ThemeProvider>
+      </Provider>
     </BrowserRouter>  
   </React.StrictMode>,
 )
