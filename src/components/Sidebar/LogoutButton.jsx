dba6a51b67fd414fa6e924de25af42ac657264d4
Granovskiy Uriy <silaveri777@gmail.net>
2024-06-10 20:34:18 +0300

    9

--- src/components/Sidebar/LogoutButton.jsx
+++ src/components/Sidebar/LogoutButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { logOut } from '../.././redux/auth/operations';
+import { logOut } from '../../redux/auth/operations';
 
 const LogoutButton = () => {
   const dispatch = useDispatch();
