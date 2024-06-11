b36e77aa9f2dbd9bc162baeaf996c2608840ab4d
Granovskiy Uriy <silaveri777@gmail.net>
2024-06-12 00:42:34 +0300

    up1

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import Sidebar from '../../components/Sidebar/Sidebar';
 
 export default function HomePage() {
     return (
-        <div className="app">
-        <Sidebar />
-        <p>Hello on HomePagePage</p>
-      </div>
-        
+
+        <p>Hello on HomePagePage</p>       
       
     )
 }
