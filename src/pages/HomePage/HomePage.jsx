f36f8d955bdcf34b52faf695b0703d738282384c
valeriiaizhyk <137833881+valeriiaizhyk@users.noreply.github.com>
2024-06-16 00:57:13 +0200

    Navigation+EditProfileModal+css

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,23 +2,25 @@ import { react } from "react";
 // import CopySideBar from "../../components/CopySideBar/CopySideBar";
 import LogoutButton from "../../components/Sidebar/Components/LogoutButton.jsx";
 // import CopyBoardItem from "../../components/CopyBoardItem/CopyBoardItem.jsx";
-import Sidebar  from '../../components/Sidebar/Components/Sidebar.jsx';
+// import Sidebar from "../../components/Sidebar/Components/Sidebar.jsx";
+// import Header from "../../components/Header/Header.jsx";
 
 export default function HomePage() {
   return (
     <div>
+      {/* <Header /> */}
       <p>Hello on HomePagePage</p>
 
       {/* <TestLogOut /> */}
 
       {/* <div className={css.container}> */}
-        {/* <CopySideBar /> */}
-        {/* <CopyBoardItem /> */}
+      {/* <CopySideBar /> */}
+      {/* <CopyBoardItem /> */}
       {/* </div> */}
 
       <LogoutButton />
 
-       {/* <Sidebar/> */}
+      {/* <Sidebar/> */}
     </div>
   );
 }
