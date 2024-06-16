aea843e738dcfc042a02aaa82805b1245640e69e
Serhii Krasovskyi <chokorik@gmail.com>
2024-06-17 02:20:06 +0300

    main_dashBoard

--- src/redux/columns/operations.js
+++ src/redux/columns/operations.js
@@ -9,14 +9,14 @@ const setBoardIdHeader = (boardId) => {
 export const fetchColumns = createAsyncThunk(
   "columns/fetchAll",
   async (currentBoard, thunkAPI) => {
-    console.log(currentBoard);
+    // console.log(currentBoard);
     try {
       const response = await axios.get("/api/columns/", {
         params: {
           boardId: currentBoard,
         },
       });
-      console.log(response.data.columns);
+      // console.log(response.data.columns);
       return response.data.columns;
     } catch (error) {
       return thunkAPI.rejectWithValue({
@@ -50,7 +50,7 @@ export const createColumn = createAsyncThunk(
   "columns/createColumn",
   async (newColumn, thunkAPI) => {
     const token = thunkAPI.getState().auth.token;
-    console.log("Token being sent:", token);
+    // console.log("Token being sent:", token);
     try {
       const response = await axios.post("/api/columns/", newColumn);
       console.log(response.data);
@@ -68,16 +68,18 @@ export const createColumn = createAsyncThunk(
 export const deleteColumn = createAsyncThunk(
   "columns/deleteColumn",
   async (columnId, thunkAPI) => {
-    const token = thunkAPI.getState().auth.token;
+    // const token = thunkAPI.getState().auth.token;
     try {
-      const response = await axios.delete(
-        `https://project-back-codewave1-rqmw.onrender.com/api/columns/${columnId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      // const response = await axios.delete(
+      //   `https://project-back-codewave1-rqmw.onrender.com/api/columns/${columnId}`,
+      //   {
+      //     headers: {
+      //       Authorization: `Bearer ${token}`,
+      //     },
+      //   }
+      // );
+      const response = await axios.delete(`/api/columns/${columnId}`);
+      // console.log(response.data);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue({
@@ -92,11 +94,13 @@ export const deleteColumn = createAsyncThunk(
 export const editColumn = createAsyncThunk(
   "columns/updateColumn",
   async ({ columnId, editColumn }, thunkAPI) => {
+    // try {
+    //   const response = await axios.put(
+    //     `https://project-back-codewave1-rqmw.onrender.com/api/columns/${columnId}`,
+    //     editColumn
+    //   );
     try {
-      const response = await axios.put(
-        `https://project-back-codewave1-rqmw.onrender.com/api/columns/${columnId}`,
-        editColumn
-      );
+      const response = await axios.put(`/api/columns/${columnId}`, editColumn);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue({
