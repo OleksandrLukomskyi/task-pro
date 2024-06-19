14cd9802a3d622ac6d41270e085f90511e09fa49
DAT <terlovoy77@gmail.com>
2024-06-19 23:12:41 +0300

    remove  comments

--- src/theme/inputFormTheme.js
+++ src/theme/inputFormTheme.js
@@ -1,51 +1,45 @@
 // Общие стили для стандартного состояния
 const commonStyles = {
-  borderColor: "var(--btn-color)",
+  borderColor: 'var(--btn-color)',
   opacity: 0.4,
 };
 
 // Общие стили для состояния фокуса
 const focusedStyles = {
-  borderColor: "var(--btn-color)",
+  borderColor: 'var(--btn-color)',
   opacity: 1,
 };
 
 // Общие стили для метки (label)
 const labelStyles = {
-  color: "var(--text-color-start-white)",
+  color: 'var(--text-color-start-white)',
   opacity: 0.3,
 };
 
 // Общие стили для ввода (input)
 const inputBaseStyles = {
-  color: "var(--text-color-start-white)",
+  color: 'var(--text-color-start-white)',
 };
 
 const inputFormTheme = {
-  "& .MuiOutlinedInput-notchedOutline": {
+  '& .MuiOutlinedInput-notchedOutline': {
     ...commonStyles,
   },
-  "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+  '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
     ...focusedStyles,
   },
-  "& .MuiInputLabel-outlined.Mui-focused": {
+  '& .MuiInputLabel-outlined.Mui-focused': {
     ...focusedStyles,
   },
-  "& .MuiInputLabel-root.Mui-focused": {
-    color: "var(--btn-color)",
+  '& .MuiInputLabel-root.Mui-focused': {
+    color: 'var(--btn-color)',
   },
-  "& .MuiInputLabel-root": {
+  '& .MuiInputLabel-root': {
     ...labelStyles,
   },
-  "& .MuiInputBase-input": {
+  '& .MuiInputBase-input': {
     ...inputBaseStyles,
-    padding: "14px 18px",
-  },
-  "& .MuiInputBase-input.Mui-filled": {
-    backgroundColor: "red",
-  },
-  "& .MuiInputBase-input.Mui -internal - autofill - selected": {
-    backgroundColor: "red",
+    padding: '14px 18px',
   },
 };
 
