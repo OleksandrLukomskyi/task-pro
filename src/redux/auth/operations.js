dc622d39ae3f4948173e5ac970e6c65eadb252a7
DAT <terlovoy77@gmail.com>
2024-06-20 16:02:41 +0300

    try to fix loading with exp. token

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,35 +1,51 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { store } from '../store';
 
 // --------------------------------------------------
-axios.defaults.baseURL = "https://project-back-codewave1-rqmw.onrender.com";
+axios.defaults.baseURL = 'https://project-back-codewave1-rqmw.onrender.com';
 // --------------------------------------------------
 
-const setAuthHeader = (token) => {
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+const setAuthHeader = token => {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 };
 
 const clearAuthHeader = () => {
-  axios.defaults.headers.common["Authorization"] = "";
+  axios.defaults.headers.common['Authorization'] = '';
+  localStorage.removeItem('token'); // Видалення токена з локального сховища
 };
+
+// інтерсептор для обробки помилок
+axios.interceptors.response.use(
+  response => response, // Якщо відповідь успішна, повертаємо її
+  error => {
+    // Якщо отримали помилку 401 (Unauthorized)
+    if (error.response?.status === 401) {
+      clearAuthHeader(); // очищуємо токен
+      store.dispatch(logOut()); //  робимо logOut
+      window.location.href = '/'; // Перенаправлення на сторінку входу
+    }
+    return Promise.reject(error);
+  }
+);
 
 // credentials - облікові дані отримані з форми
 export const register = createAsyncThunk(
-  "auth/register",
+  'auth/register',
   async (credentials, thunkAPI) => {
     try {
-      const response = await axios.post("/users/register", credentials);
+      const response = await axios.post('/users/register', credentials);
       // console.log(response.status);
       // if (!response.ok) {
       //   console.log("Email in use!");
       // }
       setAuthHeader(response.data.token);
-
+      localStorage.setItem('token', response.data.token); // збереження токена в localstorage
       return response.data;
     } catch (error) {
       const errorMessage = error.response?.data || error.message;
       if (error.response.status === 409) {
-        alert("Email in use");
+        alert('Email in use');
       }
       return thunkAPI.rejectWithValue({ message: errorMessage });
     }
@@ -37,11 +53,12 @@ export const register = createAsyncThunk(
 );
 
 export const logIn = createAsyncThunk(
-  "auth/login",
+  'auth/login',
   async (credentials, thunkAPI) => {
     try {
-      const response = await axios.post("/users/login", credentials);
+      const response = await axios.post('/users/login', credentials);
       setAuthHeader(response.data.token);
+      localStorage.setItem('token', response.data.token); // збереження токена в localstorage
       return response.data;
     } catch (error) {
       const errorMessage = error.response?.data || error.message;
@@ -51,10 +68,10 @@ export const logIn = createAsyncThunk(
 );
 
 export const editUser = createAsyncThunk(
-  "auth/edit",
+  'auth/edit',
   async (credentials, thunkAPI) => {
     try {
-      const response = await axios.patch("users/edit", credentials);
+      const response = await axios.patch('users/edit', credentials);
       setAuthHeader(response.data.token);
       return response.data;
     } catch (error) {
@@ -63,31 +80,35 @@ export const editUser = createAsyncThunk(
   }
 );
 
-export const logOut = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
+export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
   try {
-    const response = await axios.post("users/logout");
+    const response = await axios.post('users/logout');
     clearAuthHeader();
+    return response.data; //add
   } catch (error) {
     return thunkAPI.rejectWithValue(error);
   }
 });
 
 export const refreshUser = createAsyncThunk(
-  "auth/refresh",
+  'auth/refresh',
   async (_, thunkAPI) => {
     const {
       auth: { token },
     } = thunkAPI.getState();
     setAuthHeader(token);
-    const response = await axios.get("/users/current");
-    return response.data;
-  },
-  {
-    condition: (_, { getState }) => {
-      const reduxState = getState();
-      const savedToken = reduxState.auth.token;
-      return savedToken !== null;
-    },
+    try {
+      const response = await axios.get('/users/current');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+    // {
+    //   condition: (_, { getState }) => {
+    //     const reduxState = getState();
+    //     const savedToken = reduxState.auth.token;
+    //     return savedToken !== null;
+    //   },
   }
 );
 
@@ -103,9 +124,9 @@ export const refreshUser = createAsyncThunk(
 // });
 
 // ----------------------------
-export const help = createAsyncThunk("auth/support", async (_, thunkAPI) => {
+export const help = createAsyncThunk('auth/support', async (_, thunkAPI) => {
   try {
-    const response = await axios.post("/api/help/");
+    const response = await axios.post('/api/help/');
     setAuthHeader(response.data.token);
     return response.data;
   } catch (error) {
--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 import {
   register,
   logIn,
@@ -6,27 +6,29 @@ import {
   logOut,
   refreshUser,
   // userThema,
-} from "./operations";
+} from './operations.js';
+
+const initialState = {
+  user: {
+    userName: null,
+    email: null,
+    avatarURL: null,
+  },
+  token: '', //null
+  thema: 'light',
+  isLoggedIn: false,
+  isRefreshing: false,
+  loading: false,
+  error: false,
+};
 
 const authSlice = createSlice({
-  name: "auth",
-  initialState: {
-    user: {
-      userName: null,
-      email: null,
-      avatarURL: null,
-    },
-    token: null,
-    thema: "light",
-    isLoggedIn: false,
-    isRefreshing: false,
-    loading: false,
-    error: false,
-  },
+  name: 'auth',
+  initialState,
 
-  extraReducers: (builder) =>
+  extraReducers: builder =>
     builder
-      .addCase(register.pending, (state) => {
+      .addCase(register.pending, state => {
         state.loading = true;
         state.error = false;
       })
@@ -36,11 +38,11 @@ const authSlice = createSlice({
         state.isLoggedIn = true;
         state.loading = false;
       })
-      .addCase(register.rejected, (state) => {
+      .addCase(register.rejected, state => {
         state.loading = false;
         state.error = true;
       })
-      .addCase(logIn.pending, (state) => {
+      .addCase(logIn.pending, state => {
         state.loading = true;
         state.error = false;
       })
@@ -50,12 +52,12 @@ const authSlice = createSlice({
         state.isLoggedIn = true;
         state.loading = false;
       })
-      .addCase(logIn.rejected, (state) => {
+      .addCase(logIn.rejected, state => {
         state.loading = false;
         state.error = true;
       })
       // ------------------------------------------------------------------------
-      .addCase(editUser.pending, (state) => {
+      .addCase(editUser.pending, state => {
         state.loading = true;
         state.error = false;
       })
@@ -64,28 +66,34 @@ const authSlice = createSlice({
         // state.token = action.payload.token;
         state.loading = false;
       })
-      .addCase(editUser.rejected, (state) => {
+      .addCase(editUser.rejected, state => {
         state.loading = false;
         state.error = true;
       })
       // --------------------------------------------------------------------------
-      .addCase(logOut.fulfilled, (state) => {
+      .addCase(logOut.fulfilled, state => {
         state.user = {
-          name: null,
+          userName: null, // userName в initialState
           email: null,
-          password: null,
+          // password: null, // password нема в state
+          avatarURL: null,
         };
-        state.token = null;
+        state.token = ''; //null
         state.isLoggedIn = false;
         state.loading = false;
       })
-      .addCase(refreshUser.pending, (state) => {
+      .addCase(refreshUser.pending, state => {
         state.isRefreshing = true;
       })
       .addCase(refreshUser.fulfilled, (state, action) => {
         state.user = action.payload;
         state.isLoggedIn = true;
         state.isRefreshing = false;
+      })
+      // додано обробник refreshUser.rejected
+      .addCase(refreshUser.rejected, state => {
+        state.isRefreshing = false;
+        state.error = true;
       }),
 });
 
